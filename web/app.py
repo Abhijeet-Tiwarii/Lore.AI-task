c603e7fb02bf4686d9ead1853c55fc83cd45b4d8
Abhijeet-Tiwarii <abti0810@gmail.com>
2019-06-09 17:35:25 +0200

    implemented conection between our file to database and containerize

--- web/app.py
+++ web/app.py
@@ -1,14 +1,44 @@
-from flask import Flask, jsonify, json
+from flask import Flask, jsonify, json, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+
+
 app = Flask(__name__)
+api = Api(app)
+
+#client which connects to the database
+client = MongoClient("mongodb://db:27017")
+db = client.aNewDB
+statescollec = db["statescollec"]
+#upload dataset
+#into aNewDB (database) inside
+#statescollec (colllection)
+
+class Statesname(Resource):
+	def get(self):
+		state_name = statescollec.find({})[0]['states']
+		return (state_name)
+
+# class State(Resource):
+# 	def get(self):
+# 		#query with db and collection name
+# 	retSt =	statsdb.statesCollection.find(
+# 	{states[name]}
+# 	)
+
+		# get the states from the statesdb database and statesCollection collection
+#		return jsonify(retSt)
+
 
 # with open('states.json') as f:
 # 	data = json.load(f)
 
-
+#for testing
 @app.route("/")
 def hello_world():
 	return "Hello World"
 
+#for testing
 @app.route("/bye")
 def good_bye():
 	json_dic = {
@@ -17,7 +47,7 @@ def good_bye():
 	}
 	return jsonify(json_dic)
 
-
+api.add_resource(Statesname, "/state")
 # @app.route("/getstate")
 # def get_state():
 # 	for state in data['states']:
