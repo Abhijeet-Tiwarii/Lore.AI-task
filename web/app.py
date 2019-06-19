3aab65f3f588b0c9e13771996250e99f478b89e2
abti0810@gmail.com <abti0810@gmail.com>
2019-06-19 15:12:24 +0200

    retriving records from db...some changes

--- web/app.py
+++ web/app.py
@@ -9,11 +9,49 @@ api = Api(app)
 #client which connects to the database
 client = MongoClient("mongodb://db:27017")
 
-# database
+# Creating database named as "mydb"
 db = client.mydb
+
+#Inventory data has already been imported in 'inventory' collection inside 'mydb' database
+#Getting all the inventories
+@app.route('/inventoryAll', methods=['GET'])
+def get_all_inventories():
+    inventory = db.inventory
+
+    output = []
+
+    for q in inventory.find():
+        output.append({'item' : q['item'], 'qty' : q['qty'], 'status' : q['status']})
+
+    return jsonify({'result' : output})
+
+#Quering for single inventory by item
+@app.route('/inventoryOne/<item>', methods=['GET'])
+def get_one_inventories(item):
+    inventory = db.inventory
+
+    q = inventory.find_one({'item': item})
+
+    if q:
+        output = {'item': q['item'], 'qty' : q['qty'], 'status' : q['status']}
+    else:
+        output = 'No results found'
+
+    return jsonify({'result': output})
+
+if __name__=="__main__":
+	app.run(debug=True)
+
+
+
+
+
+
+
+'''
+
+
 # Created collection name: mycollection
-
-
 # emp_rec1 = {
 # 	        "name":"Mr.Geek",
 # 	        "id":24,
@@ -29,11 +67,18 @@ db = client.mydb
 # rec_id1 = collection.insert_one(emp_rec1)
 # rec_id2 = collection.insert_one(emp_rec2)
 
+# class startup(Resource):
+# 	def get(self,id):
+# 		cursor = db.inventory.find()
+# 		#cursor = collection.find({"_id":id})
+# 		return json.dump(cursor)
+
 class startup(Resource):
 	def get(self,id):
-		cursor = db.inventory.find()
-		#cursor = collection.find({"_id":id})
-		return json.dump(cursor)
+		output = []
+		for data in db.inventory.find():
+			output.append({"_id" : data['_id']})
+		return jsonify({'result':output})
 
 
 class startupall(Resource):
@@ -43,6 +88,20 @@ class startupall(Resource):
 			output.append({"item" : data['item']})
 
 		return jsonify({'result':output})
+
+class inventoryStatus(Resource):
+	def get(self):
+		output = []
+		for data in db.inventory.find():
+			output.append({"status" : data['status']})
+		return jsonify({'result':output})
+
+class inventoryIDs(Resource):
+	def get(self):
+		output = []
+		for data in db.inventory.find():
+                    return jsonify(data.inserted_ids)
+
 
 #for testing
 @app.route("/")
@@ -60,10 +119,12 @@ def good_bye():
 
 api.add_resource(startup, "/startup/<id>")
 api.add_resource(startupall, "/startup/all")
+api.add_resource(inventoryStatus, "/inventoryStatus")
+api.add_resource(inventoryIDs, "/inventoryIDs")
 # @app.route("/getstate")
 # def get_state():
 # 	for state in data['states']:
 # 		return jsonify(state['name'])
 
-if __name__=="__main__":
-	app.run(host= '0.0.0.0')
+
+'''
